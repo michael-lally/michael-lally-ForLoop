92582bbc451b93728d9f468b4a5540ffadf2f838
michael-lally <mplally@neiu.edu>
2023-02-22 19:49:34 +0000

    completed For loop

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -42,6 +42,9 @@ public class Lab {
     public String returnNumbers(int start, int end){
         String buildingString = "";
 
+        for(int i = start; i < end; i++)
+            buildingString = buildingString + i + " ";
+
 //        code here
 
         return buildingString;
